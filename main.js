689f1d9c0a8a64e8ed1961ff51f813c6afb82841
Yulia Tsernant <yulia.tsernant@d-d.me>
2023-01-22 14:20:47 -0800

    Added updated text and email

--- main.js
+++ main.js
@@ -1,46 +1,52 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  const body = document.getElementById('main');
-  const loader = document.getElementById('loader-container');
-  body.classList.remove('isLoading');
-  loader.style.display = 'none';
+window.addEventListener("DOMContentLoaded", (event) => {
+  const body = document.getElementById("main");
+  const loader = document.getElementById("loader-container");
+  body.classList.remove("isLoading");
+  loader.style.display = "none";
 });
 
 function changeNavBackground() {
-  const nav = document.getElementById('navigation');
-  nav.removeAttribute('style');
+  const nav = document.getElementById("navigation");
+  nav.removeAttribute("style");
 
   if (checkbox.checked) {
-    nav.style.backgroundColor = '#FFF9ED';
-    nav.style.transition = 'background-color 100ms ease-out';
+    nav.style.backgroundColor = "#FFF9ED";
+    nav.style.transition = "background-color 100ms ease-out";
   } else {
-    nav.style.backgroundColor = '#B47250';
-    nav.style.transition = 'background-color 100ms ease-out';
+    nav.style.backgroundColor = "#B47250";
+    nav.style.transition = "background-color 100ms ease-out";
   }
 }
 
 // Expandable Career Section
 
 function handleCareerExpand() {
-  const careerAction = document.getElementById('career-action');
-  const careerReadHide = document.getElementsByClassName('career-read-hide');
+  const careerAction = document.getElementById("career-action");
+  const careerReadHide = document.getElementsByClassName("career-read-hide");
 
-  if (careerAction.textContent === 'Read') {
-    careerAction.textContent = 'Hide';
-    careerButton.classList.add('arrowUp');
-    Array.from(careerReadHide).forEach(element => element.style.display = 'block');
+  if (careerAction.textContent === "Read") {
+    careerAction.textContent = "Hide";
+    careerButton.classList.add("arrowUp");
+    Array.from(careerReadHide).forEach(
+      (element) => (element.style.display = "block")
+    );
   } else {
-    careerAction.textContent = 'Read';
-    careerButton.classList.remove('arrowUp');
+    careerAction.textContent = "Read";
+    careerButton.classList.remove("arrowUp");
     careerAction.scrollIntoView();
 
-    Array.from(careerReadHide).forEach(element => element.removeAttribute('style'));
+    Array.from(careerReadHide).forEach((element) =>
+      element.removeAttribute("style")
+    );
   }
 }
 
 // Photo Gallery
 
 function setupCoverflow() {
-  let coverflowContainers = Array.from(document.getElementsByClassName('coverflow'));
+  let coverflowContainers = Array.from(
+    document.getElementsByClassName("coverflow")
+  );
 
   coverflowContainers.forEach((containerElement) => {
     const coverflow = {};
@@ -48,11 +54,13 @@ function setupCoverflow() {
 
     //capture coverflow elements
     coverflow.container = containerElement;
-    coverflow.images = Array.from(containerElement.getElementsByClassName('coverflow__image'));
+    coverflow.images = Array.from(
+      containerElement.getElementsByClassName("coverflow__image")
+    );
     coverflow.position = Math.floor(coverflow.images.length / 2) + 1;
 
     //set indicies on images
-    coverflow.images.forEach(function(coverflowImage, i) {
+    coverflow.images.forEach(function (coverflowImage, i) {
       coverflowImage.dataset.coverflowIndex = i + 1;
     });
 
@@ -73,13 +81,19 @@ function setupCoverflow() {
     }
 
     function setNextImage() {
-      coverflow.position = Math.min(coverflow.images.length, coverflow.position + 1);
+      coverflow.position = Math.min(
+        coverflow.images.length,
+        coverflow.position + 1
+      );
       coverflow.container.dataset.coverflowPosition = coverflow.position;
       changeImagePosition(coverflow.position - 1, coverflow.images);
     }
 
     function jumpToImage(evt) {
-      coverflow.position = Math.min(coverflow.images.length, Math.max(1, evt.target.dataset.coverflowIndex));
+      coverflow.position = Math.min(
+        coverflow.images.length,
+        Math.max(1, evt.target.dataset.coverflowIndex)
+      );
       coverflow.container.dataset.coverflowPosition = coverflow.position;
       changeImagePosition(coverflow.position - 1, coverflow.images);
     }
@@ -94,35 +108,44 @@ function setupCoverflow() {
           break;
       }
     }
-    prevArrow.addEventListener('click', setPrevImage);
-    nexArrow.addEventListener('click', setNextImage);
-    coverflow.images.forEach(function(image) {
-      image.addEventListener('click', jumpToImage);
+    prevArrow.addEventListener("click", setPrevImage);
+    nexArrow.addEventListener("click", setNextImage);
+    coverflow.images.forEach(function (image) {
+      image.addEventListener("click", jumpToImage);
     });
-    window.addEventListener('keyup', onKeyPress);
+    window.addEventListener("keyup", onKeyPress);
   });
 }
 const generateOffsetOrSizeArray = (min, step, length) => {
   return new Array(length).fill(0).map((_, i) => min + step * i);
-}
+};
 
 const changeImagePosition = (coverFlowPosition, images) => {
   // Need to change size in CSS as well
   const imgSize = 25;
   const step = imgSize / 100;
   const offsetRight = imgSize / 1000;
-  const offsetLeft = -1 - (offsetRight + (step * (images.length - 1)));
+  const offsetLeft = -1 - (offsetRight + step * (images.length - 1));
   const minSize = -(imgSize / (imgSize * 10));
   const sizeStep = imgSize / (imgSize * 10);
   const maxZIndex = images.length + 1;
-  
-  const transformXNext = generateOffsetOrSizeArray(offsetRight, step, images.length);
-  const transformXPrev = generateOffsetOrSizeArray(offsetLeft, step, images.length);
+
+  const transformXNext = generateOffsetOrSizeArray(
+    offsetRight,
+    step,
+    images.length
+  );
+  const transformXPrev = generateOffsetOrSizeArray(
+    offsetLeft,
+    step,
+    images.length
+  );
   const sizes = generateOffsetOrSizeArray(minSize, sizeStep, images.length);
 
   images.forEach((image, i) => {
     let offset, scale, zIndex;
-    if (i < coverFlowPosition) { //4
+    if (i < coverFlowPosition) {
+      //4
       //image is on the left from center
       const idx = coverFlowPosition - i;
       offset = `${imgSize * transformXPrev[transformXPrev.length - idx]}vw`;
@@ -133,7 +156,7 @@ const changeImagePosition = (coverFlowPosition, images) => {
       scale = sizes[sizes.length - (i - coverFlowPosition)];
       zIndex = maxZIndex - (i - coverFlowPosition);
     } else {
-      offset = '-50%';
+      offset = "-50%";
       scale = 1;
       zIndex = maxZIndex;
     }
@@ -142,37 +165,42 @@ const changeImagePosition = (coverFlowPosition, images) => {
     image.style.webkitTransform = `translateX(${offset}) scale(${scale})`;
     image.style.zIndex = zIndex;
   });
-}
+};
 
 function handleInputChange(input) {
   if (input.value.length > 0) {
-    input.classList.remove('missing-info');
+    input.classList.remove("missing-info");
   } else {
-    input.classList.add('missing-info');
-    input.placeholder = 'Please, fill in the form'
+    input.classList.add("missing-info");
+    input.placeholder = "Please, fill in the form";
   }
 }
 
 function sendEmail() {
-  const textElement = document.getElementById('comment');
-  const nameElement = document.getElementById('name');
-  const providedEmail = document.getElementById('email');
-  const textWithName = textElement.value + '\n\n' + ` Name: ${nameElement.value} \n Please, reply to: ${providedEmail.value}`;
-  const subject = 'Hi Hanna, an email from your website'
+  const textElement = document.getElementById("comment");
+  const nameElement = document.getElementById("name");
+  const providedEmail = document.getElementById("email");
+  const textWithName =
+    textElement.value +
+    "\n\n" +
+    ` Name: ${nameElement.value} \n Please, reply to: ${providedEmail.value}`;
+  const subject = "Hi Hanna, an email from your website";
 
-  const mailToLink = `mailto:hrybkovahanna@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(textWithName)}`;
+  const mailToLink = `mailto:hrybkova.hanna@gmail.com?subject=${encodeURIComponent(
+    subject
+  )}&body=${encodeURIComponent(textWithName)}`;
   window.location.href = mailToLink;
 }
 
 // Navigation hamburger that triggers it's background change
-const checkbox = document.getElementById('menu-toggle');
-// Arrow that expands career text 
-const careerButton = document.getElementById('career-arrow');
+const checkbox = document.getElementById("menu-toggle");
+// Arrow that expands career text
+const careerButton = document.getElementById("career-arrow");
 // Event listeners
-checkbox.addEventListener('click', changeNavBackground);
+checkbox.addEventListener("click", changeNavBackground);
 // Triggers a function that shows content on click
-careerButton.addEventListener('click', handleCareerExpand); 
-const submitButton = document.getElementById('submit-btn');
-submitButton.addEventListener('click', sendEmail);
+careerButton.addEventListener("click", handleCareerExpand);
+const submitButton = document.getElementById("submit-btn");
+submitButton.addEventListener("click", sendEmail);
 
 setupCoverflow();
